baa3e92065b3679c7d99e3766527534eb6188d13
Giovana Gabriele <168477853+giovanagabi27@users.noreply.github.com>
2025-03-17 17:04:35 -0300

    att

---
+++ projeto_integrador/app/comanda/page.js
@@ -0,0 +1,15 @@
+'use cliet'
+ import "./cliente.css"
+
+
+
+function comanda () {
+    return (  
+        <div className=" body" >
+            <h1> 🍺 Bem vindo ao Boteco do João 🍺</h1>
+
+        </div>
+    );
+}
+
+export default comanda ;
