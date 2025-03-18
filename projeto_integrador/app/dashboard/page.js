d12924048eb7f5adcfc7f8af601deae8cc0bf3fe
Arthurad11 <tut798068@gmail.com>
2025-03-18 15:27:59 -0300

    dashboard

---
+++ projeto_integrador/app/dashboard/page.js
@@ -0,0 +1,31 @@
+import Menu from "../components/Menu";
+
+import "./dashboard.css";
+
+function Dashboard() {
+
+
+
+    return ( 
+        <div>
+            <Menu />
+            <aside>
+                <ul>
+                    <li className="first" >Dashboard</li>
+                    <li>Faturamento</li>
+                    <li>Estoque</li>
+                    <li>Pedidos</li>
+                    <li>Clientes</li>
+                    <li>Funcionários</li>
+                    <li><button className="deslogar"><img width="12px" src="https://cdn-icons-png.flaticon.com/512/4043/4043198.png"/> Sair da Conta</button></li>
+                </ul>
+            </aside>
+            <main>
+                
+            </main>
+
+        </div>
+    );
+}
+
+export default Dashboard;
