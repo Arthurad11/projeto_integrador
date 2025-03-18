15f99e524d370dd3b8e9e26e61f64bd1d79129ed
EduardoSilvahh <eduardothobias13@gmail.com>
2025-03-18 17:05:05 -0300

    aTT

--- projeto_integrador/app/dashboard/page.js
+++ projeto_integrador/app/dashboard/page.js
@@ -13,17 +13,45 @@ function Dashboard() {
             <Menu />
             <aside>
                 <ul>
-                    <li className="first" >Dashboard</li>
-                    <li>Faturamento</li>
-                    <li>Estoque</li>
-                    <li>Pedidos</li>
-                    <li>Clientes</li>
-                    <li>Funcionários</li>
+                    <li onClick={()=> {}} className="first" >Dashboard</li>
+                    <li onClick={()=> {}}>Faturamento</li>
+                    <li onClick={()=> {}}>Estoque</li>
+                    <li onClick={()=> {}}>Pedidos</li>
+                    <li onClick={()=> {}}>Clientes</li>
+                    <li onClick={()=> {}}>Funcionários</li>
                     <li><button className="deslogar"><img width="12px" src="https://cdn-icons-png.flaticon.com/512/4043/4043198.png"/> Sair da Conta</button></li>
                 </ul>
             </aside>
             <main>
-                
+                {1
+
+
+                }
+
+                {2
+
+
+                }
+
+                {3
+
+
+                }
+
+                {4
+
+
+                }
+
+                {5
+
+
+                }
+
+                {6
+
+
+                }
             </main>
 
         </div>
