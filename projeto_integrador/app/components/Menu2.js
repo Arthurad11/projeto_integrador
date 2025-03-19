aa9ff0c43d58e96b371c7b0b21e57f57f784d4ed
Arthurad11 <tut798068@gmail.com>
2025-03-19 14:28:03 -0300

    alinhamento do conteudo -dashboard

--- projeto_integrador/app/components/Menu2.js
+++ projeto_integrador/app/components/Menu2.js
@@ -1,36 +1,160 @@
-"use client"
+"use client";
 
+import { useState } from "react";
 import "./menu2.css";
+import Estoque from "./Estoque";
+import Clientes from "./Clientes";
+import Funcionarios from "./Funcionarios";
 
 function Menu2() {
+	function toggleMenu() {
+		const menu = document.querySelector("nav ul");
+		menu.classList.toggle("active");
+	}
 
-    function toggleMenu() {
-        const menu = document.querySelector('nav ul');
-        menu.classList.toggle('active');
-    }
+	const [dashboard, alteraDashboard] = useState();
+	const [faturamento, alteraFaturamento] = useState();
+	const [estoque, alteraEstoque] = useState();
+	const [pedidos, alteraPedidos] = useState();
+	const [clientes, alteraClientes] = useState();
+	const [funcionarios, alteraFuncionarios] = useState();
 
 	return (
-		<header>
-			<nav>
-                <a>Bar do Jaum</a>
+		<div>
+			<header>
+				<nav>
+					<a>Bar do Jaum</a>
 
-                <div className="mobile-menu" onClick={toggleMenu}>
-                    <div className="line1"></div>
-                    <div className="line2"></div>
-                    <div className="line3"></div>
-                </div>
+					<div className="mobile-menu" onClick={toggleMenu}>
+						<div className="line1"></div>
+						<div className="line2"></div>
+						<div className="line3"></div>
+					</div>
 
-				<ul className="menu">
-					<li onClick={()=> {}}>Dashboard</li>
-                    <li onClick={()=> {}}>Faturamento</li>
-                    <li onClick={()=> {}}>Estoque</li>
-                    <li onClick={()=> {}}>Pedidos</li>
-                    <li onClick={()=> {}}>Clientes</li>
-                    <li onClick={()=> {}}>Funcionários</li>
-                    <li><button className="deslogar"><img width="12px" src="https://cdn-icons-png.flaticon.com/512/4043/4043198.png"/> Sair da Conta</button></li>
-				</ul>
-			</nav>
-		</header>
+					<ul className="menu">
+						<li
+							onClick={() => {
+								alteraDashboard(true);
+								alteraFaturamento(false);
+								alteraEstoque(false);
+								alteraPedidos(false);
+								alteraClientes(false);
+								alteraFuncionarios(false);
+							}}
+							className="first">
+							Dashboard
+						</li>
+
+						<li
+							onClick={() => {
+								alteraDashboard(false);
+								alteraFaturamento(true);
+								alteraEstoque(false);
+								alteraPedidos(false);
+								alteraClientes(false);
+								alteraFuncionarios(false);
+							}}>
+							Faturamento
+						</li>
+
+						<li
+							onClick={() => {
+								alteraDashboard(false);
+								alteraFaturamento(false);
+								alteraEstoque(true);
+								alteraPedidos(false);
+								alteraClientes(false);
+								alteraFuncionarios(false);
+							}}>
+							Estoque
+						</li>
+
+						<li
+							onClick={() => {
+								alteraDashboard(false);
+								alteraFaturamento(false);
+								alteraEstoque(false);
+								alteraPedidos(true);
+								alteraClientes(false);
+								alteraFuncionarios(false);
+							}}>
+							Pedidos
+						</li>
+
+						<li
+							onClick={() => {
+								alteraDashboard(false);
+								alteraFaturamento(false);
+								alteraEstoque(false);
+								alteraPedidos(false);
+								alteraClientes(true);
+								alteraFuncionarios(false);
+							}}>
+							Clientes
+						</li>
+
+						<li
+							onClick={() => {
+								alteraDashboard(false);
+								alteraFaturamento(false);
+								alteraEstoque(false);
+								alteraPedidos(false);
+								alteraClientes(false);
+								alteraFuncionarios(true);
+							}}>
+							Funcionários
+						</li>
+						<li>
+							<button className="deslogar">
+								<img width="12px" src="https://cdn-icons-png.flaticon.com/512/4043/4043198.png" /> Sair da Conta
+							</button>
+						</li>
+					</ul>
+				</nav>
+			</header>
+            <main>
+                {/* {
+                    alteraDashboard == true && 
+
+                    
+                    
+
+                } */}
+
+                {/* {
+                    alteraFaturamento == true &&
+
+
+                } */}
+
+                {
+                    estoque == true &&
+                    <Estoque/>
+
+
+                }
+
+                {
+
+
+
+                }
+
+                {
+                    clientes == true &&
+                    <Clientes/>
+
+
+                }
+
+                {
+                    funcionarios == true &&
+                    <Funcionarios/>
+
+
+                }
+            </main>
+		</div>
 	);
 }
 
--- projeto_integrador/app/dashboard/page.js
+++ projeto_integrador/app/dashboard/page.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Clientes from "../components/Clientes";
 import Estoque from "../components/Estoque";
@@ -8,36 +8,101 @@ import { useState } from "react";
 import "./dashboard.css";
 
 function Dashboard() {
-    const[dashboard , alteraDashboard] = useState()
-    const[faturamento , alteraFaturamento] = useState()
-    const[estoque, alteraEstoque] = useState()
-    const[pedidos, alteraPedidos] = useState()
-    const[clientes, alteraClientes] = useState()
-    const[funcionarios, alteraFuncionarios] = useState()
+	const [dashboard, alteraDashboard] = useState();
+	const [faturamento, alteraFaturamento] = useState();
+	const [estoque, alteraEstoque] = useState();
+	const [pedidos, alteraPedidos] = useState();
+	const [clientes, alteraClientes] = useState();
+	const [funcionarios, alteraFuncionarios] = useState();
 
+	return (
+		<div>
+			<Menu2 />
+			<div className="container">
+				<aside>
+					<ul>
+						<li
+							onClick={() => {
+								alteraDashboard(true);
+								alteraFaturamento(false);
+								alteraEstoque(false);
+								alteraPedidos(false);
+								alteraClientes(false);
+								alteraFuncionarios(false);
+							}}
+							className="first">
+							Dashboard
+						</li>
 
-    return ( 
-        <div>
-            <Menu2 />
-            <aside>
-                <ul>
-                    <li onClick={()=> {alteraDashboard(true); alteraFaturamento(false); alteraEstoque(false);alteraPedidos(false);alteraClientes(false);alteraFuncionarios(false)}} className="first" >Dashboard</li>
+						<li
+							onClick={() => {
+								alteraDashboard(false);
+								alteraFaturamento(true);
+								alteraEstoque(false);
+								alteraPedidos(false);
+								alteraClientes(false);
+								alteraFuncionarios(false);
+							}}>
+							Faturamento
+						</li>
 
-                    <li onClick={()=> {alteraDashboard(false); alteraFaturamento(true); alteraEstoque(false);alteraPedidos(false);alteraClientes(false);alteraFuncionarios(false)}}>Faturamento</li>
+						<li
+							onClick={() => {
+								alteraDashboard(false);
+								alteraFaturamento(false);
+								alteraEstoque(true);
+								alteraPedidos(false);
+								alteraClientes(false);
+								alteraFuncionarios(false);
+							}}>
+							Estoque
+						</li>
 
-                    <li onClick={()=> {alteraDashboard(false); alteraFaturamento(false); alteraEstoque(true);alteraPedidos(false);alteraClientes(false);alteraFuncionarios(false)}}>Estoque</li>
+						<li
+							onClick={() => {
+								alteraDashboard(false);
+								alteraFaturamento(false);
+								alteraEstoque(false);
+								alteraPedidos(true);
+								alteraClientes(false);
+								alteraFuncionarios(false);
+							}}>
+							Pedidos
+						</li>
 
-                    <li onClick={()=> {alteraDashboard(false); alteraFaturamento(false); alteraEstoque(false);alteraPedidos(true);alteraClientes(false);alteraFuncionarios(false)}}>Pedidos</li>
+						<li
+							onClick={() => {
+								alteraDashboard(false);
+								alteraFaturamento(false);
+								alteraEstoque(false);
+								alteraPedidos(false);
+								alteraClientes(true);
+								alteraFuncionarios(false);
+							}}>
+							Clientes
+						</li>
 
-                    <li onClick={()=> {alteraDashboard(false); alteraFaturamento(false); alteraEstoque(false);alteraPedidos(false);alteraClientes(true);alteraFuncionarios(false)}}>Clientes</li>
+						<li
+							onClick={() => {
+								alteraDashboard(false);
+								alteraFaturamento(false);
+								alteraEstoque(false);
+								alteraPedidos(false);
+								alteraClientes(false);
+								alteraFuncionarios(true);
+							}}>
+							Funcionários
+						</li>
 
-                    <li onClick={()=> {alteraDashboard(false); alteraFaturamento(false); alteraEstoque(false);alteraPedidos(false);alteraClientes(false);alteraFuncionarios(true)}}>Funcionários</li>
-
-                    <li><button className="deslogar"><img width="12px" src="https://cdn-icons-png.flaticon.com/512/4043/4043198.png"/> Sair da Conta</button></li>
-                </ul>
-            </aside>
-            <main>
-                {/* {
+						<li>
+							<button className="deslogar">
+								<img width="12px" src="https://cdn-icons-png.flaticon.com/512/4043/4043198.png" /> Sair da Conta
+							</button>
+						</li>
+					</ul>
+				</aside>
+				<main>
+					{/* {
                     alteraDashboard == true && 
 
                     
@@ -45,42 +110,23 @@ function Dashboard() {
 
                 } */}
 
-                {/* {
+					{/* {
                     alteraFaturamento == true &&
 
 
                 } */}
 
-                {
-                    estoque == true &&
-                    <Estoque/>
+					{estoque == true && <Estoque />}
 
+					{}
 
-                }
+					{clientes == true && <Clientes />}
 
-                {
-
-
-
-                }
-
-                {
-                    clientes == true &&
-                    <Clientes/>
-
-
-                }
-
-                {
-                    funcionarios == true &&
-                    <Funcionarios/>
-
-
-                }
-            </main>
-
-        </div>
-    );
+					{funcionarios == true && <Funcionarios />}
+				</main>
+			</div>
+		</div>
+	);
 }
 
 export default Dashboard;
