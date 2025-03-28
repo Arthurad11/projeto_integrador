15c0f7be59a346e7f793921d22a30de4f81ace6e
EduardoSilvahh <eduardothobias13@gmail.com>
2025-03-28 15:16:58 -0300

    alteração css tabelas

--- projeto_integrador/app/components/produtos/Estoque.js
+++ projeto_integrador/app/components/produtos/Estoque.js
@@ -9,6 +9,7 @@ function Estoque() {
                     <th>ID</th>
                     <th>Categoria</th>
                     <th>Produto</th>
+                    <th>Preço</th>
                 </tr>
             </thead>
             <tbody>
@@ -16,21 +17,25 @@ function Estoque() {
                     <td>1</td>
                     <td>Cerveja</td>
                     <td>Skol</td>
+                    <td>R$ 4,00</td>
                 </tr>
                 <tr>
                     <td>2</td>
                     <td>Cerveja</td>
                     <td>Império</td>
+                    <td>R$ 5,00</td>
                 </tr>
                 <tr>
                     <td>3</td>
                     <td>Destilados</td>
                     <td>Red Label</td>
+                    <td>R$ 90,00</td>
                 </tr>
                 <tr>
                     <td>4</td>
                     <td>Destilados</td>
                     <td>Tanqueray</td>
+                    <td>R$ 100,00</td>
                 </tr>
 
             </tbody>
