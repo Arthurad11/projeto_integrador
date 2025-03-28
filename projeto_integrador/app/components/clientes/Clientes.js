73a260be17add9e1a51b863ae0b004a2d0a704c2
Arthurad11 <tut798068@gmail.com>
2025-03-28 15:16:18 -0300

    tabela clientes - css

--- projeto_integrador/app/components/clientes/Clientes.js
+++ projeto_integrador/app/components/clientes/Clientes.js
@@ -6,24 +6,24 @@ function Clientes() {
             <table>
                 <thead>
                     <tr>
-                        <th>ID</th>
-                        <th>Nome</th>
-                        <th>Email</th>
+                        <th className="id">ID</th>
+                        <th className="nome">Nome</th>
+                        <th className="email">Email</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td>1</td>
+                        <td id="id">1</td>
                         <td>João Silva</td>
                         <td>joaosil@gmail.com</td>
                     </tr>
                     <tr>
-                        <td>2</td>
+                        <td id="id">2</td>
                         <td>Maria Oliveira</td>
                         <td>maoliveira@email.com</td>
                     </tr>
                     <tr>
-                        <td>3</td>
+                        <td id="id">3</td>
                         <td>Carlos Santos</td>
                         <td>c.santos@outlook.com</td>
                     </tr>
