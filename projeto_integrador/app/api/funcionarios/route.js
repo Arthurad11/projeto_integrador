53348d008499f300287c1e4a9630ed24da611c77
EduardoSilvahh <eduardothobias13@gmail.com>
2025-04-08 17:01:18 -0300

    api funcionarios

---
+++ projeto_integrador/app/api/funcionarios/route.js
@@ -0,0 +1,36 @@
+import conexao from "@/app/lib/conexao"
+
+export async function GET (){
+
+    const query = `SELECT * FROM usuarios WHERE funcionario = 1`;
+    const [results] = await conexao.execute ( query )
+
+    return new Response(
+        JSON.stringify(results),
+        {
+            status: 200,
+            headers: { "Content-Type": "application/json"}
+        }
+    )
+
+}
+
+export async function POST ( request ){
+    
+    const body = await request.json()
+
+    const query = `
+        INSERT INTO usuarios
+        (nome, email, senha, registro)
+        VALUES
+        (?, ?, ?, ?);
+    `
+
+    const [results] = await conexao.execute(
+        query,
+        [body.nome, body.email, body.senha]
+    )
+
+    return new Response( JSON.stringify(results.insertId) )
+
+}
--- projeto_integrador/app/components/funcionarios/Funcionarios.js
+++ projeto_integrador/app/components/funcionarios/Funcionarios.js
@@ -1,38 +1,75 @@
+import { useEffect, useState } from "react"
 import "./funcionarios.css"
+import axios from "axios"
 
 function Funcionarios() {
+
+    const [pesquisa, alteraPesquisa] = useState("")
+
+    const [clientes, alteraClientes] = useState([])
+
+
+    async function buscaPorID( id ){
+        const response = await axios.get("http://localhost:3000/api/funcionarios/"+id)
+        alteraClientes(response.data)
+    }
+
+
+    async function buscaTodos() {
+        const response = await axios.get("http://localhost:3000/api/funcionarios")
+        alteraClientes(response.data)
+    }
+
+    function formataData( valor ){
+		let data = valor.split("T")[0]
+		let hora = valor.split("T")[1]
+
+		data = data.split("-")
+		data = data.reverse()
+		data = data.join("/")
+
+		hora = hora.split(".")[0]
+		hora = hora.split(":")
+		hora = hora[0]+":"+hora[1]
+
+		return data+" às "+hora
+
+	}
+
+    useEffect(()=> {
+        buscaTodos();
+    },[])
+
     return ( 
         <div>
             <table>
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Nome</th>
-                        <th>Login</th>
-                        
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>João Silva</td>
-                        <td>Jsilva01</td>
-                        
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Maria Oliveira</td>
-                        <td>MaOliveira02</td>
+						<thead>
+							<tr>
+								<td>ID</td>
+								<td>Nome</td>
+								<td>Email</td>						
+								<td>Registro</td>
+							</tr>
+						</thead>
+						<tbody>
+							{
+								clientes.map( i=>
+									<tr key={i.id}>
+										<td>{i.id}</td>
+										<td>{i.nome}</td>
+										<td>{i.email}</td>
+										<td>{ formataData(i.registro) }</td>
+										<td>
+											<button onClick={()=> montaEdicao(i)} >Editar</button>
+											<button onClick={()=> removeProduto(i.id)}>Remover</button>
+										</td>
+									</tr>
 
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>Carlos Santos</td>
-                        <td>SantCarlos03</td>
+								)
+							}
+						</tbody>
 
-                    </tr>
-                </tbody>
-            </table>
+					</table>
 
         </div>
 
