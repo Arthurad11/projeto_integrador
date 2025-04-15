f4862f8bbeb532be023785f2693d1ab7f26f79b3
Giovana Gabriele <168477853+giovanagabi27@users.noreply.github.com>
2025-04-15 13:15:44 -0300

    cliente

--- projeto_integrador/app/login/login_cliente/page.js
+++ projeto_integrador/app/login/login_cliente/page.js
@@ -1,49 +1,29 @@
-'use client'
-import React, { useState } from 'react';
-import axios from 'axios';
-import './logincliente.css'; // Assume que o CSS está nesse arquivo
+import React from 'react';
+import './logincliente.css';
 
 const Card = () => {
-  const [title, setTitle] = useState("Cameron Williamson");
-  const [subtitle, setSubtitle] = useState("Web Development");
-
-  const handleClick = async () => {
-    try {
-      const response = await axios.get('https://jsonplaceholder.typicode.com/users/1');
-      setTitle(response.data.name);
-      setSubtitle(response.data.email);
-    } catch (error) {
-      console.error('Erro ao buscar dados:', error);
-    }
-  };
-
   return (
     <div className="card">
       <div className="card__img">
-        {/* Insere o mesmo SVG ou use uma imagem externa */}
-        <svg width="100%" xmlns="http://www.w3.org/2000/svg">
-          <rect height="450" width="540" fill="#ffffff"></rect>
-          {/* Adicione o restante do SVG aqui ou substitua por uma imagem */}
+        {/* SVG de fundo */}
+        <svg xmlns="http://www.w3.org/2000/svg" width="100%">
+          {/* ... (mantenha todo o conteúdo do SVG aqui) ... */}
         </svg>
       </div>
 
       <div className="card__avatar">
+        {/* SVG do avatar */}
         <svg viewBox="0 0 128 128" xmlns="http://www.w3.org/2000/svg">
-          <circle r="60" fill="#ff8475" cy="64" cx="64" />
-          {/* Reduza o SVG ou use <img src="avatar.svg" /> */}
+          {/* ... (mantenha todo o conteúdo do avatar aqui) ... */}
         </svg>
       </div>
 
-      <div className="card__title">{title}</div>
-      <div className="card__subtitle">{subtitle}</div>
+      <div className="card__title">Cameron Williamson</div>
+      <div className="card__subtitle">Web Development</div>
 
       <div className="card__wrapper">
-        <button className="card__btn" onClick={handleClick}>
-          Buscar
-        </button>
-        <button className="card__btn card__btn-solid" onClick={handleClick}>
-          Atualizar
-        </button>
+        <button className="card__btn">Button</button>
+        <button className="card__btn card__btn-solid">Button</button>
       </div>
     </div>
   );
