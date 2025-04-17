be483de1ca458152ba6fafc88677154ae6cecde4
EduardoSilvahh <eduardothobias13@gmail.com>
2025-04-17 14:47:26 -0300

    margin top produtos

--- projeto_integrador/app/produtos/page.js
+++ projeto_integrador/app/produtos/page.js
@@ -9,164 +9,204 @@ import axios from "axios";
 import host from "../lib/host";
 
 function Produtos() {
-	//busca cerveja
-	const [cerveja, setCerveja] = useState();
-	async function buscaCerveja() {
-		const response = await axios.get(host+"/produtos/cerveja");
-		setCerveja(response.data);
-	}
+  //busca cerveja
+  const [cerveja, setCerveja] = useState();
+  async function buscaCerveja() {
+    const response = await axios.get(host + "/produtos/cerveja");
+    setCerveja(response.data);
+  }
 
-	//busca chopp
-	const [chopp, setChopp] = useState();
-	async function buscaChopp() {
-		const response = await axios.get(host+"/produtos/chopp");
-		setChopp(response.data);
-	}
+  //busca chopp
+  const [chopp, setChopp] = useState();
+  async function buscaChopp() {
+    const response = await axios.get(host + "/produtos/chopp");
+    setChopp(response.data);
+  }
 
-	//busca drinks
-	const [drinks, setDrinks] = useState();
-	async function buscaDrinks() {
-		const response = await axios.get(host+"/produtos/drinks");
-		setDrinks(response.data);
-	}
+  //busca drinks
+  const [drinks, setDrinks] = useState();
+  async function buscaDrinks() {
+    const response = await axios.get(host + "/produtos/drinks");
+    setDrinks(response.data);
+  }
 
-	//busca sucos
-	const [sucos, setSucos] = useState();
-	async function buscaSucos() {
-		const response = await axios.get(host+"/produtos/sucos");
-		setSucos(response.data);
-	}
+  //busca sucos
+  const [sucos, setSucos] = useState();
+  async function buscaSucos() {
+    const response = await axios.get(host + "/produtos/sucos");
+    setSucos(response.data);
+  }
 
-	//busca refrigerantes
-	const [refrigerantes, setRefrigerantes] = useState();
-	async function buscaRefrigerantes() {
-		const response = await axios.get(host+"/produtos/refrigerantes");
-		setRefrigerantes(response.data);
-	}
+  //busca refrigerantes
+  const [refrigerantes, setRefrigerantes] = useState();
+  async function buscaRefrigerantes() {
+    const response = await axios.get(host + "/produtos/refrigerantes");
+    setRefrigerantes(response.data);
+  }
 
-	//busca bebidas em geral
-	const [bebidas, setBebidas] = useState();
-	async function buscaBebidas() {
-		const response = await axios.get(host+"/produtos/bebidas");
-		setBebidas(response.data);
-	}
+  //busca bebidas em geral
+  const [bebidas, setBebidas] = useState();
+  async function buscaBebidas() {
+    const response = await axios.get(host + "/produtos/bebidas");
+    setBebidas(response.data);
+  }
 
-	//busca espetinhos
-	const [espetinhos, setEspetinhos] = useState();
-	async function buscaEspetinhos() {
-		const response = await axios.get(host+"/produtos/espetinhos");
-		setEspetinhos(response.data);
-	}
+  //busca espetinhos
+  const [espetinhos, setEspetinhos] = useState();
+  async function buscaEspetinhos() {
+    const response = await axios.get(host + "/produtos/espetinhos");
+    setEspetinhos(response.data);
+  }
 
-	//busca porções
-	const [porcoes, setPorcoes] = useState();
-	async function buscaPorcoes() {
-		const response = await axios.get(host+"/produtos/porcoes");
-		setPorcoes(response.data);
-	}
+  //busca porções
+  const [porcoes, setPorcoes] = useState();
+  async function buscaPorcoes() {
+    const response = await axios.get(host + "/produtos/porcoes");
+    setPorcoes(response.data);
+  }
 
-	useEffect(() => {
-		buscaCerveja();
-		buscaChopp();
-		buscaDrinks();
-		buscaSucos();
-		buscaRefrigerantes();
-		buscaBebidas();
-		buscaEspetinhos();
-		buscaPorcoes();
-	}, []);
+  useEffect(() => {
+    buscaCerveja();
+    buscaChopp();
+    buscaDrinks();
+    buscaSucos();
+    buscaRefrigerantes();
+    buscaBebidas();
+    buscaEspetinhos();
+    buscaPorcoes();
+  }, []);
 
-	return (
-		<div>
-			<Menu />
-			<br />
-			<br />
+  return (
+    <div>
+      <Menu />
+      <br />
+      <br />
+      <div className="bottom">
+        <h1>Produtos</h1>
+        <br />
 
-			<h1>Produtos</h1>
-			<br />
+        <div className="produtos">
+          <h2>Cerveja</h2>
+          <br />
 
-			<div className="produtos">
-				<h2>Cerveja</h2>
-				<br />
+          <div className="item-container">
+            {cerveja?.map((i) => (
+              <Produto
+                key={i.nome}
+                nome={i.nome}
+                preco={i.preco.toFixed(2)}
+                imagem={`${i.imagem}`}
+              />
+            ))}
+          </div>
 
-				<div className="item-container">
-					{cerveja?.map((i) => (
-						<Produto key={i.nome} nome={i.nome} preco={i.preco.toFixed(2)} imagem={`${i.imagem}`} />
-					))}
-				</div>
+          <br />
+          <br />
 
-				<br />
-				<br />
+          <h2>Chopp</h2>
+          <br />
 
-				<h2>Chopp</h2>
-				<br />
+          <div className="item-container">
+            {chopp?.map((i) => (
+              <Produto
+                key={i.nome}
+                nome={i.nome}
+                preco={i.preco.toFixed(2)}
+                imagem={`${i.imagem}`}
+              />
+            ))}
+          </div>
+          <br />
+          <br />
 
-				<div className="item-container">
-					{chopp?.map((i) => (
-						<Produto key={i.nome} nome={i.nome} preco={i.preco.toFixed(2)} imagem={`${i.imagem}`} />
-					))}
-				</div>
-				<br />
-				<br />
+          <h2>Drinks</h2>
+          <br />
 
-				<h2>Drinks</h2>
-				<br />
+          <div className="item-container">
+            {drinks?.map((i) => (
+              <Produto
+                key={i.nome}
+                nome={i.nome}
+                preco={i.preco.toFixed(2)}
+                imagem={`${i.imagem}`}
+              />
+            ))}
+          </div>
 
-				<div className="item-container">
-					{drinks?.map((i) => (
-						<Produto key={i.nome} nome={i.nome} preco={i.preco.toFixed(2)} imagem={`${i.imagem}`} />
-					))}
-				</div>
+          <h2>Sucos</h2>
+          <br />
 
-				<h2>Sucos</h2>
-				<br />
+          <div className="item-container">
+            {sucos?.map((i) => (
+              <Produto
+                key={i.nome}
+                nome={i.nome}
+                preco={i.preco.toFixed(2)}
+                imagem={`${i.imagem}`}
+              />
+            ))}
+          </div>
 
-				<div className="item-container">
-					{sucos?.map((i) => (
-						<Produto key={i.nome} nome={i.nome} preco={i.preco.toFixed(2)} imagem={`${i.imagem}`} />
-					))}
-				</div>
+          <h2>Refrigerantes</h2>
+          <br />
 
-				<h2>Refrigerantes</h2>
-				<br />
+          <div className="item-container">
+            {refrigerantes?.map((i) => (
+              <Produto
+                key={i.nome}
+                nome={i.nome}
+                preco={i.preco.toFixed(2)}
+                imagem={`${i.imagem}`}
+              />
+            ))}
+          </div>
 
-				<div className="item-container">
-					{refrigerantes?.map((i) => (
-						<Produto key={i.nome} nome={i.nome} preco={i.preco.toFixed(2)} imagem={`${i.imagem}`} />
-					))}
-				</div>
+          <h2>Bebidas em Geral</h2>
+          <br />
 
-				<h2>Bebidas em Geral</h2>
-				<br />
+          <div className="item-container">
+            {bebidas?.map((i) => (
+              <Produto
+                key={i.nome}
+                nome={i.nome}
+                preco={i.preco.toFixed(2)}
+                imagem={`${i.imagem}`}
+              />
+            ))}
+          </div>
 
-				<div className="item-container">
-					{bebidas?.map((i) => (
-						<Produto key={i.nome} nome={i.nome} preco={i.preco.toFixed(2)} imagem={`${i.imagem}`} />
-					))}
-				</div>
+          <h2>Espetinhos</h2>
+          <br />
 
-				<h2>Espetinhos</h2>
-				<br />
+          <div className="item-container">
+            {espetinhos?.map((i) => (
+              <Produto
+                key={i.nome}
+                nome={i.nome}
+                preco={i.preco.toFixed(2)}
+                imagem={`${i.imagem}`}
+              />
+            ))}
+          </div>
 
-				<div className="item-container">
-					{espetinhos?.map((i) => (
-						<Produto key={i.nome} nome={i.nome} preco={i.preco.toFixed(2)} imagem={`${i.imagem}`} />
-					))}
-				</div>
+          <h2>Porções</h2>
+          <br />
 
-				<h2>Porções</h2>
-				<br />
-
-				<div className="item-container">
-					{porcoes?.map((i) => (
-						<Produto key={i.nome} nome={i.nome} preco={i.preco.toFixed(2)} imagem={`${i.imagem}`} />
-					))}
-				</div>
-			</div>
-
-			<br />
-		</div>
-	);
+          <div className="item-container">
+            {porcoes?.map((i) => (
+              <Produto
+                key={i.nome}
+                nome={i.nome}
+                preco={i.preco.toFixed(2)}
+                imagem={`${i.imagem}`}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+      <br />
+    </div>
+  );
 }
 
 export default Produtos;
